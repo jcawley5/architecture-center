028d7aee938c81999330f3044f40dff539b5d1f3
Julian Schambeck <julian.schambeck@sap.com>
2025-02-06 14:38:36 +0100

    title requires vertical shift. inner workings of layout get confusing...

--- src/_scripts/_export-drawios.js
+++ src/_scripts/_export-drawios.js
@@ -13,8 +13,10 @@ const DRAWIO_CLI_MAC_BINARY = '/Applications/draw.io.app/Contents/MacOS/draw.io'
 // assuming script is in src/_scripts/
 const SEARCH_DIR = __dirname + '/../../docs/ref-arch';
 const SAP_LOGO = __dirname + '/../../static/img/logo.svg';
-const FONT_SIZE_DEFAULT = 18;
 const SVG_BACKGROUND_COLOR = '#ffffff';
+const URL = "https://architecture.cloud.sap";
+
+const single = process.argv[2];
 
 if (!DOCKER) {
     try {
@@ -29,7 +31,8 @@ const files = readdirSync(SEARCH_DIR, { recursive: true });
 const drawios = files.filter((file) => file.match(/\.drawio$/));
 log(`Found ${drawios.length} drawios to export to svg\n`);
 
-const transforms = {};
+// TODO: make his const again later
+let transforms = {};
 // drawio as in RA0001/drawio/Events-to-business-actions-framework.drawio
 for (const drawio of drawios) {
     // Events-to-business-actions-framework part
@@ -38,6 +41,14 @@ for (const drawio of drawios) {
     transforms[`${SEARCH_DIR}/${drawio}`] = svg;
 }
 
+if (single) {
+    log("single is", single)
+    const k = Object.keys(transforms).find(k => k.includes(single));
+    transforms = { [k]: transforms[k] };
+    log("now it's", JSON.stringify(transforms))
+}
+
+// throw new Error("stop")
 // export all drawios to svgs
 for (const [input, out] of Object.entries(transforms)) {
     const cmd = prepareCommand(input, out);
@@ -74,15 +85,24 @@ for (const [drawioPath, svgPath] of Object.entries(transforms)) {
     const height = parseInt(viewBox[3]);
     const width = parseInt(viewBox[2]);
     // finding these exact values is a bit trial and error
-    const pad = 10;
+    const pad = 20; // additional padding to the one drawio itself already has
     viewBox[0] = -pad;
     viewBox[1] = -pad;
     // pad*2 to have ~same padding right/left and top/bottom
     viewBox[2] = width + pad * 2;
-    viewBox[3] = height + 74 + pad * 2;
-    const textX = 182;
-    const logoDownScale = 0.35;
+    const logoHeight = 52;
+    // x coordinate of title is bottom of text, so need to shift everything to keep padding top
+    const yShift = 40;
+    // actual title height is 25 with font size 22, but this gets us closer
+    // to the top padding we want
+    const titleHeight = 25 - 5;
+    // height increase = logo height + pad top/left + pad between logo and diagram
+    // (+ necessary vertical shift for now cause title)
+    viewBox[3] = height + logoHeight + pad * 3 + titleHeight + yShift;
 
+    const logoWidth = 106;
+    const logoY = height + pad + yShift;
+    const textX = logoWidth + pad;
     try {
         const iso = execSync(`git log -1 --format=%cd --date=iso "${drawioPath}"`);
         const lastUpdate = new Date(iso).toLocaleDateString('en-US', {
@@ -91,18 +111,30 @@ for (const [drawioPath, svgPath] of Object.entries(transforms)) {
             day: 'numeric',
         });
 
+        const revert = 30;
         const logo = readFileSync(SAP_LOGO, 'utf8');
-        const mark = `<text x="${textX}" y="${height + 34}" font-family="Arial" font-size="${FONT_SIZE_DEFAULT}">
-                        Last update <tspan font-style="italic">${lastUpdate}</tspan>
+        const mark = `<text x="0" y="${titleHeight}" font-family="Arial" font-weight="bold" font-size="22">
+                        Solution Diagram Title
                     </text>
-                    <g transform="translate(70, ${height + 14}) scale(${logoDownScale} ${logoDownScale})">
-                        <image href="data:image/svg+xml;base64,${Buffer.from(logo).toString('base64')}" />
-                    </g>`;
+                    <text x="${textX}" y="${logoY + 20 + yShift - revert}" font-family="Arial" font-weight="bold" font-size="20">
+                        Architecture Center
+                    </text>
+                    <text x="${textX}" y="${logoY + logoHeight - 4 + yShift - revert}" font-family="Arial" font-style="italic" font-size="16">
+                        Last update on ${lastUpdate}
+                    </text>
+                    <g transform="translate(0, ${logoY + yShift - revert})">
+                        <image width="${logoWidth}" height="${logoHeight}" href="data:image/svg+xml;base64,${Buffer.from(logo).toString('base64')}" />
+                    </g>
+                    <text x="${width/2}" y="${logoY + 36 + yShift - revert}" font-family="Arial" font-size="18">
+                        ${URL}
+                    </text>`;
 
         const bg = `<rect x="${-pad}" y="${-pad}" width="${viewBox[2]}" height="${viewBox[3]}" fill="${SVG_BACKGROUND_COLOR}"/>`;
         svg = svg
             // leave svg opening tag unchanged, but add rect element to set background color
             .replace(/<svg([^>]*)>/, '<svg$1>' + bg)
+            // this g contains the diagram
+            .replace("<g>", `<g transform="translate(0, ${yShift})">`)
             .replace(/<\/svg>$/, mark + '</svg>')
             .replace(/viewBox="([^"]*)"/, `viewBox="${viewBox.join(' ')}"`)
             // height attribute was set to same as viewbox height, so update it too
