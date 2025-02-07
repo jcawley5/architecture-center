cacdab9f36467fa648dcd1dbb305e5adc6efd644
Julian Schambeck <julian.schambeck@sap.com>
2025-02-07 02:47:43 +0100

    wrap up layout

--- src/_scripts/_export-drawios.js
+++ src/_scripts/_export-drawios.js
@@ -84,25 +84,20 @@ for (const [drawioPath, svgPath] of Object.entries(transforms)) {
     const viewBox = svg.match(/viewBox="([^"]*)"/)[1].split(' ');
     const height = parseInt(viewBox[3]);
     const width = parseInt(viewBox[2]);
-    // finding these exact values is a bit trial and error
+    // finding these exact values is a bit trial and error..
     const pad = 20; // additional padding to the one drawio itself already has
     viewBox[0] = -pad;
     viewBox[1] = -pad;
-    // pad*2 to have ~same padding right/left and top/bottom
-    viewBox[2] = width + pad * 2;
-    const logoHeight = 52;
-    // x coordinate of title is bottom of text, so need to shift everything to keep padding top
-    const yShift = 40;
-    // actual title height is 25 with font size 22, but this gets us closer
-    // to the top padding we want
-    const titleHeight = 25 - 5;
-    // height increase = logo height + pad top/left + pad between logo and diagram
-    // (+ necessary vertical shift for now cause title)
-    viewBox[3] = height + logoHeight + pad * 3 + titleHeight + yShift;
-
-    const logoWidth = 106;
-    const logoY = height + pad + yShift;
-    const textX = logoWidth + pad;
+    // add padding left/right
+    viewBox[2] = width + pad*2 + 100;
+    const hLogo = 52;
+    const wLogo = 106;
+    // margin top of logo
+    const mtLogo = 30;
+    const yLogo = height + mtLogo;
+    const shift = 38;
+    viewBox[3] = height + pad*2 + mtLogo + hLogo + shift;
+    const textX = wLogo + pad;
     try {
         const iso = execSync(`git log -1 --format=%cd --date=iso "${drawioPath}"`);
         const lastUpdate = new Date(iso).toLocaleDateString('en-US', {
@@ -113,28 +108,30 @@ for (const [drawioPath, svgPath] of Object.entries(transforms)) {
 
         const revert = 30;
         const logo = readFileSync(SAP_LOGO, 'utf8');
-        const mark = `<text x="0" y="${titleHeight}" font-family="Arial" font-weight="bold" font-size="22">
+        const mark = `<text x="0" y="${pad}" font-family="Arial" font-weight="bold" font-size="22">
                         Solution Diagram Title
                     </text>
-                    <text x="${textX}" y="${logoY + 20 + yShift - revert}" font-family="Arial" font-weight="bold" font-size="20">
+                    <g transform="translate(0, ${shift})">
+                    <text x="${textX}" y="${yLogo + 20}" font-family="Arial" font-weight="bold" font-size="20">
                         Architecture Center
                     </text>
-                    <text x="${textX}" y="${logoY + logoHeight - 4 + yShift - revert}" font-family="Arial" font-style="italic" font-size="16">
+                    <text x="${textX}" y="${yLogo + hLogo - 4}" font-family="Arial" font-style="italic" font-size="16">
                         Last update on ${lastUpdate}
                     </text>
-                    <g transform="translate(0, ${logoY + yShift - revert})">
-                        <image width="${logoWidth}" height="${logoHeight}" href="data:image/svg+xml;base64,${Buffer.from(logo).toString('base64')}" />
+                    <g transform="translate(0, ${yLogo})">
+                        <image width="${wLogo}" height="${hLogo}" href="data:image/svg+xml;base64,${Buffer.from(logo).toString('base64')}" />
                     </g>
-                    <text x="${width/2}" y="${logoY + 36 + yShift - revert}" font-family="Arial" font-size="18">
+                    <text x="${width/2}" y="${yLogo + 36}" font-family="Arial" font-size="18">
                         ${URL}
-                    </text>`;
+                    </text>
+                    </g>`;
 
         const bg = `<rect x="${-pad}" y="${-pad}" width="${viewBox[2]}" height="${viewBox[3]}" fill="${SVG_BACKGROUND_COLOR}"/>`;
         svg = svg
             // leave svg opening tag unchanged, but add rect element to set background color
             .replace(/<svg([^>]*)>/, '<svg$1>' + bg)
             // this g contains the diagram
-            .replace("<g>", `<g transform="translate(0, ${yShift})">`)
+            .replace("<g>", `<g transform="translate(0, ${shift})">`)
             .replace(/<\/svg>$/, mark + '</svg>')
             .replace(/viewBox="([^"]*)"/, `viewBox="${viewBox.join(' ')}"`)
             // height attribute was set to same as viewbox height, so update it too
