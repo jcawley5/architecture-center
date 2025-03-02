d389193e1a163b98b94ea800910d14428db58f74
navyakhurana <navyakhurana2000@gmail.com>
2025-03-02 20:16:14 +0530

    Modified positioning of navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
                 showLastUpdateTime: true,
                 showLastUpdateAuthor: true,
                 onInlineTags: 'throw',
-                editUrl: 'https://github.com/SAP/architecture-center/edit/main/'
+                editUrl: 'https://github.com/SAP/architecture-center/edit/main/',
             },
         ],
         [
@@ -77,7 +77,7 @@ const config: Config = {
                 },
             };
         },
-        './src/plugins/asset-types'
+        './src/plugins/asset-types',
     ],
 
     i18n: {
@@ -129,222 +129,222 @@ const config: Config = {
     ],
 
     themeConfig: {
-            metadata: [
+        metadata: [
+            {
+                name: 'description',
+                content:
+                    'SAP Architecture Center: Optimize cloud deployments with a well-architected framework, collaborative experiences, and cost-saving tools for improved cost of ownership.',
+            },
+            {
+                name: 'keywords',
+                content: 'sap, btp, architecture, center, hyperscaler, reference',
+            },
+        ],
+        zoom: {
+            selector: '.markdown :not(em) > img:not(.no-zoom)',
+            background: {
+                light: 'rgb(255, 255, 255)',
+                dark: 'rgb(50, 50, 50)',
+            },
+            config: {
+                margin: 16,
+                container: {
+                    top: 50,
+                },
+            },
+        },
+        docs: {
+            sidebar: {
+                autoCollapseCategories: true,
+                hideable: true,
+            },
+        },
+        // Announcement Bar
+        announcementBar: {
+            id: 'internal-prototype',
+            content:
+                '<b>The Architecture Center is still under development, and some features are not yet available. <a href="mailto:paa@sap.com?subject=[Architecture Center] Message&body=Dear PAA team,%0D%0DI would like to contact you because:%0D%0D< Just want to say Hi or make a suggestion? Enter your message here! >%0D%0DThank you!%0D%0DRegards,%0D%0D< your_name >">We welcome your feedback</a>.</b>',
+            backgroundColor: '#0A6ED1',
+            textColor: '#FFFFFF',
+            isCloseable: true,
+        },
+        navbar: {
+            title: 'Architecture Center',
+            hideOnScroll: false,
+            logo: {
+                alt: 'SAP',
+                src: 'img/logo.svg',
+            },
+            items: [
                 {
-                    name: 'description',
-                    content:
-                        'SAP Architecture Center: Optimize cloud deployments with a well-architected framework, collaborative experiences, and cost-saving tools for improved cost of ownership.',
+                    to: 'blog',
+                    label: "What's new",
+                    position: 'left',
                 },
                 {
-                    name: 'keywords',
-                    content: 'sap, btp, architecture, center, hyperscaler, reference',
+                    to: '/community/intro',
+                    label: 'Community of Practice',
+                    position: 'left',
+                },
+                {
+                    to: '/guidance/intro',
+                    label: 'Guidance Framework',
+                    position: 'left',
+                },
+                {
+                    type: 'dropdown',
+                    label: 'SAP Viewpoints',
+                    position: 'right',
+                    items: [
+                        {
+                            type: 'html',
+                            value: '<strong>Explore</strong>',
+                        },
+                        {
+                            to: '/explore',
+                            label: 'Explore all Reference Architectures',
+                            sidebarId: 'explore',
+                        },
+                        {
+                            type: 'html',
+                            value: '<hr style="margin: 0.3rem 0;">',
+                        },
+                        {
+                            type: 'html',
+                            value: '<strong>Partners</strong>',
+                        },
+                        {
+                            type: 'docSidebar',
+                            sidebarId: 'aws',
+                            label: 'Amazon Web Services',
+                        },
+                        {
+                            type: 'docSidebar',
+                            sidebarId: 'azure',
+                            label: 'Microsoft Azure',
+                        },
+                        {
+                            type: 'docSidebar',
+                            sidebarId: 'gcp',
+                            label: 'Google Cloud Platform',
+                        },
+                        {
+                            type: 'html',
+                            value: '<hr style="margin: 0.3rem 0;">',
+                        },
+                        {
+                            type: 'html',
+                            value: '<strong>Technology Domains</strong>',
+                        },
+                        {
+                            type: 'docSidebar',
+                            sidebarId: 'appdev',
+                            label: 'Application Development & Automation',
+                        },
+                        {
+                            type: 'docSidebar',
+                            sidebarId: 'ai',
+                            label: 'Artificial Intelligence',
+                        },
+                        {
+                            type: 'docSidebar',
+                            sidebarId: 'data',
+                            label: 'Data & Analytics',
+                        },
+                        {
+                            type: 'docSidebar',
+                            sidebarId: 'integration',
+                            label: 'Integration',
+                        },
+                        {
+                            type: 'docSidebar',
+                            sidebarId: 'security',
+                            label: 'Security',
+                        },
+                    ],
+                },
+                {
+                    href: 'https://github.com/SAP/architecture-center',
+                    position: 'right',
+                    className: 'navbar-item-github',
+                    'aria-label': 'GitHub repository',
+                    title: 'Visit GitHub Repository',
                 },
             ],
-            zoom: {
-                selector: '.markdown :not(em) > img:not(.no-zoom)',
-                background: {
-                    light: 'rgb(255, 255, 255)',
-                    dark: 'rgb(50, 50, 50)',
+        },
+        footer: {
+            style: 'light',
+            links: [
+                {
+                    title: 'GitHub',
+                    items: [
+                        {
+                            label: 'GitHub Issues',
+                            href: 'https://github.com/SAP/architecture-center/issues/new/choose',
+                        },
+                    ],
                 },
-                config: {
-                    margin: 16,
-                    container: {
-                        top: 50,
-                    },
+                {
+                    title: 'SAP Community',
+                    items: [
+                        {
+                            label: 'Enterprise Architecture',
+                            href: 'https://community.sap.com/t5/enterprise-architecture/gh-p/Enterprise-Architecture',
+                        },
+                        {
+                            label: 'Blogs',
+                            href: 'https://community.sap.com/t5/all-sap-community-blogs/ct-p/all-blogs',
+                        },
+                        {
+                            label: 'Q&A',
+                            href: 'https://community.sap.com/',
+                        },
+                    ],
                 },
-            },
-            docs: {
-                sidebar: {
-                    autoCollapseCategories: true,
-                    hideable: true,
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'SAP Discovery Center',
+                            href: 'https://discovery-center.cloud.sap/index.html',
+                        },
+                        {
+                            label: 'SAP Architecture Center (GitHub Repository)',
+                            href: 'https://github.com/SAP/architecture-center',
+                        },
+                    ],
                 },
-            },
-            // Announcement Bar
-            announcementBar: {
-                id: 'internal-prototype',
-                content:
-                    '<b>The Architecture Center is still under development, and some features are not yet available. <a href="mailto:paa@sap.com?subject=[Architecture Center] Message&body=Dear PAA team,%0D%0DI would like to contact you because:%0D%0D< Just want to say Hi or make a suggestion? Enter your message here! >%0D%0DThank you!%0D%0DRegards,%0D%0D< your_name >">We welcome your feedback</a>.</b>',
-                backgroundColor: '#0A6ED1',
-                textColor: '#FFFFFF',
-                isCloseable: true,
-            },
-            navbar: {
-                title: 'Architecture Center',
-                hideOnScroll: false,
-                logo: {
-                    alt: 'SAP',
-                    src: 'img/logo.svg',
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()}  SAP SE or SAP affiliate company. All rights reserved.`,
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+            magicComments: [
+                // Remember to extend the default highlight class name as well!
+                {
+                    className: 'theme-code-block-highlighted-line',
+                    line: 'highlight-next-line',
+                    block: { start: 'highlight-start', end: 'highlight-end' },
                 },
-                items: [
-                    {
-                        to: 'blog',
-                        label: "What's new",
-                        position: 'right',
-                    },
-                    {
-                        to: '/guidance/intro',
-                        label: 'Guidance Framework',
-                        position: 'right',
-                    },
-                    {
-                        to: '/community/intro',
-                        label: 'Community of Practice',
-                        position: 'right',
-                    },
-                    {
-                        type: 'dropdown',
-                        label: 'SAP Viewpoints',
-                        position: 'right',
-                        items: [
-                            {
-                                type: 'html',
-                                value: '<strong>Explore</strong>',
-                            },
-                            {
-                                to: '/explore',
-                                label: 'Explore all Reference Architectures',
-                                sidebarId: 'explore',
-                            },
-                            {
-                                type: 'html',
-                                value: '<hr style="margin: 0.3rem 0;">',
-                            },
-                            {
-                                type: 'html',
-                                value: '<strong>Partners</strong>',
-                            },
-                            {
-                                type: 'docSidebar',
-                                sidebarId: 'aws',
-                                label: 'Amazon Web Services',
-                            },
-                            {
-                                type: 'docSidebar',
-                                sidebarId: 'azure',
-                                label: 'Microsoft Azure',
-                            },
-                            {
-                                type: 'docSidebar',
-                                sidebarId: 'gcp',
-                                label: 'Google Cloud Platform',
-                            },
-                            {
-                                type: 'html',
-                                value: '<hr style="margin: 0.3rem 0;">',
-                            },
-                            {
-                                type: 'html',
-                                value: '<strong>Technology Domains</strong>',
-                            },
-                            {
-                                type: 'docSidebar',
-                                sidebarId: 'appdev',
-                                label: 'Application Development & Automation',
-                            },
-                            {
-                                type: 'docSidebar',
-                                sidebarId: 'ai',
-                                label: 'Artificial Intelligence',
-                            },
-                            {
-                                type: 'docSidebar',
-                                sidebarId: 'data',
-                                label: 'Data & Analytics',
-                            },
-                            {
-                                type: 'docSidebar',
-                                sidebarId: 'integration',
-                                label: 'Integration',
-                            },
-                            {
-                                type: 'docSidebar',
-                                sidebarId: 'security',
-                                label: 'Security',
-                            },
-                        ],
-                    },
-                    {
-                        href: 'https://github.com/SAP/architecture-center',
-                        position: 'right',
-                        className: 'navbar-item-github',
-                        'aria-label': 'GitHub repository',
-                        title: 'Visit GitHub Repository',
-                    },
-                ],
-            },
-            footer: {
-                style: 'light',
-                links: [
-                    {
-                        title: 'GitHub',
-                        items: [
-                            {
-                                label: 'GitHub Issues',
-                                href: 'https://github.com/SAP/architecture-center/issues/new/choose',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'SAP Community',
-                        items: [
-                            {
-                                label: 'Enterprise Architecture',
-                                href: 'https://community.sap.com/t5/enterprise-architecture/gh-p/Enterprise-Architecture',
-                            },
-                            {
-                                label: 'Blogs',
-                                href: 'https://community.sap.com/t5/all-sap-community-blogs/ct-p/all-blogs',
-                            },
-                            {
-                                label: 'Q&A',
-                                href: 'https://community.sap.com/',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            {
-                                label: 'SAP Discovery Center',
-                                href: 'https://discovery-center.cloud.sap/index.html',
-                            },
-                            {
-                                label: 'SAP Architecture Center (GitHub Repository)',
-                                href: 'https://github.com/SAP/architecture-center',
-                            },
-                        ],
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()}  SAP SE or SAP affiliate company. All rights reserved.`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-                magicComments: [
-                    // Remember to extend the default highlight class name as well!
-                    {
-                        className: 'theme-code-block-highlighted-line',
-                        line: 'highlight-next-line',
-                        block: { start: 'highlight-start', end: 'highlight-end' },
-                    },
-                    {
-                        className: 'code-block-hidden',
-                        line: 'hide-next-line',
-                        block: { start: 'hide-start', end: 'hide-end' },
-                    },
-                    {
-                        className: 'theme-code-block-added-line',
-                        line: 'added-line',
-                        block: { start: 'added-start', end: 'added-end' },
-                    },
-                    {
-                        className: 'theme-code-block-removed-line',
-                        line: 'removed-line',
-                        block: { start: 'removed-start', end: 'removed-end' },
-                    },
-                ],
-            },
-        } satisfies Preset.ThemeConfig,
+                {
+                    className: 'code-block-hidden',
+                    line: 'hide-next-line',
+                    block: { start: 'hide-start', end: 'hide-end' },
+                },
+                {
+                    className: 'theme-code-block-added-line',
+                    line: 'added-line',
+                    block: { start: 'added-start', end: 'added-end' },
+                },
+                {
+                    className: 'theme-code-block-removed-line',
+                    line: 'removed-line',
+                    block: { start: 'removed-start', end: 'removed-end' },
+                },
+            ],
+        },
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
