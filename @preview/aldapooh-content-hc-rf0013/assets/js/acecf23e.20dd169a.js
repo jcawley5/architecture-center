ab4911e28ecfc4372dbb9338c4c72f852222ee63
1726GIT_USER <1726GIT_USER_EMAIL>
2025-03-27 19:01:38 +0000

    pr build ➝ gh pages

---
+++ @preview/aldapooh-content-hc-rf0013/assets/js/acecf23e.20dd169a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/aldapooh-content-hc-rf0013/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/aldapooh-content-hc-rf0013/blog/authors"}')}}]);
