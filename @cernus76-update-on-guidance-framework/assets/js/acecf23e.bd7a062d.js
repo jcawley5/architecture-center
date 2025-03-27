ec611494a507d0e38319431461e5fe7f40128de7
Navya Khurana <64686397+navyakhurana@users.noreply.github.com>
2025-03-27 21:55:38 +0530

    Delete @cernus76-update-on-guidance-framework directory

--- @cernus76-update-on-guidance-framework/assets/js/acecf23e.bd7a062d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@cernus76-update-on-guidance-framework/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@cernus76-update-on-guidance-framework/blog/authors"}')}}]);
