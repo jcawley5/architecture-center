943e3837a15fe70039c9c7c81654e03e96243044
1775GIT_USER <1775GIT_USER_EMAIL>
2025-03-31 14:17:52 +0000

    pr build ➝ gh pages

---
+++ @preview/jmsrpp-jmsrpp-hc-11/assets/js/acecf23e.4f77d180.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/jmsrpp-jmsrpp-hc-11/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/jmsrpp-jmsrpp-hc-11/blog/authors"}')}}]);
