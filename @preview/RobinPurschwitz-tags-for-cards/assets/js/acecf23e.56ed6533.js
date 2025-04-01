14938c471af9b53752bd4578963e37af6a5596a5
1776GIT_USER <1776GIT_USER_EMAIL>
2025-04-01 11:55:27 +0000

    pr build ➝ gh pages

---
+++ @preview/RobinPurschwitz-tags-for-cards/assets/js/acecf23e.56ed6533.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/RobinPurschwitz-tags-for-cards/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/RobinPurschwitz-tags-for-cards/blog/authors"}')}}]);
