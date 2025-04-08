8726d022956ff951d2823cef82ef98fe3be841fb
1762GIT_USER <1762GIT_USER_EMAIL>
2025-04-08 09:04:28 +0000

    pr build ➝ gh pages

---
+++ @preview/xammaxx-fix/173-footer/assets/js/acecf23e.57a0c7b3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/xammaxx-fix/173-footer/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/xammaxx-fix/173-footer/blog/authors"}')}}]);
