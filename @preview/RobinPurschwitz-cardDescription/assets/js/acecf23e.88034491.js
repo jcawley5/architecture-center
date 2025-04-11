89dee84816f48b644894d5dc63be4abe8102df68
1703GIT_USER <1703GIT_USER_EMAIL>
2025-04-11 09:37:22 +0000

    pr build ➝ gh pages

---
+++ @preview/RobinPurschwitz-cardDescription/assets/js/acecf23e.88034491.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/RobinPurschwitz-cardDescription/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/RobinPurschwitz-cardDescription/blog/authors"}')}}]);
