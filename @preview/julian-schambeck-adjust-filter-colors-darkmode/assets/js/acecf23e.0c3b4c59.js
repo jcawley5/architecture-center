172ff0cf1fc9ae3301e5697e71f2ea119d42d654
1812GIT_USER <1812GIT_USER_EMAIL>
2025-04-11 10:40:34 +0000

    pr build ➝ gh pages

---
+++ @preview/julian-schambeck-adjust-filter-colors-darkmode/assets/js/acecf23e.0c3b4c59.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/julian-schambeck-adjust-filter-colors-darkmode/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/julian-schambeck-adjust-filter-colors-darkmode/blog/authors"}')}}]);
