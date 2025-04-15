1f7c29a9e02238dc67bc27135b90db268249c60b
1763GIT_USER <1763GIT_USER_EMAIL>
2025-04-15 11:06:11 +0000

    pr build ➝ gh pages

---
+++ @preview/julian-schambeck-subtle-footer-colors/assets/js/acecf23e.0b7f5bd6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/julian-schambeck-subtle-footer-colors/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/julian-schambeck-subtle-footer-colors/blog/authors"}')}}]);
