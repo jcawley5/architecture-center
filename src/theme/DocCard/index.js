a4a230ef6755dc180aee7841de242123752336af
Navya Khurana <64686397+navyakhurana@users.noreply.github.com>
2025-04-17 20:52:01 +0530

    [Bug-fix] Card: Date is slightly cut off to the right (#235)
    * [Bug-fix] Card: Date is slightly cut off to the right
    
    * Updates from editor
    
    * Updates from editor
    
    * Updates from editor
    
    ---------
    
    Co-authored-by: johannagonnzdz <162330474+johannagonnzdz@users.noreply.github.com>

--- src/theme/DocCard/index.js
+++ src/theme/DocCard/index.js
@@ -4,16 +4,16 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState, useRef, useEffect} from "react";
 import {
   findFirstSidebarItemLink,
-} from '@docusaurus/plugin-content-docs/client';
-import {usePluralForm} from '@docusaurus/theme-common';
-import {translate} from '@docusaurus/Translate';
-import {Card, Text, Icon, FlexBox, Tag, Title, Popover, ExpandableText} from '@ui5/webcomponents-react';
+} from "@docusaurus/plugin-content-docs/client";
+import {usePluralForm} from "@docusaurus/theme-common";
+import {translate} from "@docusaurus/Translate";
+import {Card, Text, Icon, FlexBox, Tag, Title, Popover} from "@ui5/webcomponents-react";
 import "@ui5/webcomponents-icons/dist/dimension";
 import "@ui5/webcomponents-icons/dist/action";
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 function useCategoryItemsPlural() {
   const {selectMessage} = usePluralForm();
@@ -22,10 +22,10 @@ function useCategoryItemsPlural() {
       count,
       translate(
         {
-          message: '1 item|{count} items',
-          id: 'theme.docs.DocCard.categoryDescription.plurals',
+          message: "1 item|{count} items",
+          id: "theme.docs.DocCard.categoryDescription.plurals",
           description:
-            'The default description for a category card in the generated index about how many items this category includes',
+            "The default description for a category card in the generated index about how many items this category includes",
         },
         {count},
       ),
@@ -105,9 +105,9 @@ function CardLayout({ href, title, description, tags, lastUpdate, item }) {
 
     if (description.length > Math.round((cardWidth / 360) * 200)) {
         const _description = description.slice(0, Math.round((cardWidth / 360) * 200));
-        const lastSpaceIndex = _description.lastIndexOf(' ');
-        const lastCharacterIsDot = _description.slice(-1) === '.';
-        setReadableDescription(_description.slice(0, lastSpaceIndex) + (lastCharacterIsDot ? '..' : '...'));
+        const lastSpaceIndex = _description.lastIndexOf(" ");
+        const lastCharacterIsDot = _description.slice(-1) === ".";
+        setReadableDescription(_description.slice(0, lastSpaceIndex) + (lastCharacterIsDot ? ".." : "..."));
     } else {
         setReadableDescription(description);
     }
@@ -150,196 +150,186 @@ function CardLayout({ href, title, description, tags, lastUpdate, item }) {
     <Card
       ref={card}
       style={{
-        height: '290px',
-        cursor: 'pointer',
-        borderRadius: '20px',
-        overflow: 'visible',
-        transition: 'all 0.3s ease-in-out',
-        backgroundColor: 'var(--ifm-card-background)',
-        boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
+        height: "290px",
+        cursor: "pointer",
+        borderRadius: "20px",
+        overflow: "visible",
+        transition: "all 0.3s ease-in-out",
+        backgroundColor: "var(--ifm-card-background)",
+        boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
       }}
       onClick={(event) => {
         if (
-          event.target.tagName === 'UI5-TAG' ||
-          event.target.tagName === 'UI5-POPOVER'
+          event.target.tagName === "UI5-TAG" ||
+          event.target.tagName === "UI5-POPOVER"
         )
           return;
         window.location.href = href;
       }}
       onMouseEnter={(e) => {
-        e.currentTarget.style.backgroundColor = 'var(--ifm-hover-overlay)';
-        e.currentTarget.style.boxShadow = '0px 6px 16px rgba(0, 112, 242, 0.4)';
-        e.currentTarget.style.transform = 'translateY(-3px)';
+        e.currentTarget.style.backgroundColor = "var(--ifm-hover-overlay)";
+        e.currentTarget.style.boxShadow = "0px 6px 16px rgba(0, 112, 242, 0.4)";
+        e.currentTarget.style.transform = "translateY(-3px)";
       }}
       onMouseLeave={(e) => {
-        e.currentTarget.style.backgroundColor = 'var(--ifm-card-background)';
-        e.currentTarget.style.boxShadow = '0 2px 6px rgba(0, 0, 0, 0.1)';
-        e.currentTarget.style.transform = 'none';
+        e.currentTarget.style.backgroundColor = "var(--ifm-card-background)";
+        e.currentTarget.style.boxShadow = "0 2px 6px rgba(0, 0, 0, 0.1)";
+        e.currentTarget.style.transform = "none";
       }}
     >
-      <FlexBox direction='Column' justifyContent='End' style={{height: "290px", margin: 0, padding: 0}} gap={0}>
+    <FlexBox direction="Column" justifyContent="End" style={{height: "290px"}}>
     {/* Custom Banner Image */}
     <FlexBox direction="Column">
       <div
         style={{
-          height: '50px',
-          backgroundImage: `url('${
+          height: "50px",
+          backgroundImage: `url("${
               item.customProps?.isGuidance 
-                ? '/img/Card_header_green.jpg' 
-                : '/img/Card_header_blue.jpg'
-            }')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
+                ? "/img/Card_header_green.jpg" 
+                : "/img/Card_header_blue.jpg"
+            }")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       />
-
-      {/* Blue Left Accent */}
     </FlexBox>
-    <FlexBox direction='Column' style={{height: "175px"}}>
+    {/* Blue Left Accent and Card Content */}    
+    <FlexBox direction="Column" style={{height: "175px"}}>
       <FlexBox justifyContent="Start" alignItems="Center" style={{height: "60px"}}>
         <div
           style={{
-            height: '40px',
-            width: '6px',
-            minWidth: '6px',
-            borderTopRightRadius: '4px',
-            borderBottomRightRadius: '4px',
-            backgroundColor: item.customProps?.isGuidance ? '#06B400': '#0070F2',
+            height: "40px",
+            width: "6px",
+            minWidth: "6px",
+            borderTopRightRadius: "4px",
+            borderBottomRightRadius: "4px",
+            backgroundColor: item.customProps?.isGuidance ? "#06B400": "#0070F2",
             marginTop: "10px"
           }}
         />
-        <Title style={{padding: '0 16px', paddingTop: '10px', cursor: "pointer", fontSize: "18px"}}>{readableTitle}</Title>
+        <Title style={{padding: "0 16px", paddingTop: "10px", cursor: "pointer", fontSize: "18px"}}>{readableTitle}</Title>
       </FlexBox>
+    <FlexBox style={{padding: "16px 16px 0px 16px", textAlign: "left", cursor: "pointer"}}>
+      <Text>{readableDescription}</Text>
+    </FlexBox>
+    </FlexBox>
+    {/* Tags container */}
+    <FlexBox direction="Column" justifyContent="SpaceBetween" alignItems="Start" gap={10} style={{width: "100%", height: "65px"}}>
     <FlexBox
       style={{
-        padding: '16px 16px 0px 16px',
-          textAlign: 'left',
-          cursor: 'pointer',
-          margin: 0,
+        padding: "0 16px 0px 16px",
+        margin: 0,
+        bottom: 0,
+        width: "100%"
       }}
+      direction="Row"
+      gap={3}
+      alignItems="End"
+      justifyContent="Start"
     >
-    <Text>
-      {readableDescription}
-    </Text>
+      {compressedTags.map((tag, index) => (
+        <a href={"/docs/tags/" + tag.tag} key={index}>
+          <Tag
+            design="Information"
+            hideStateIcon
+            className={styles.tag}
+            title={tag.description || tag.label}
+            style={{borderRadius: "8px"}}
+          >
+            <div style={{textWrap: "nowrap"}}>
+              {tag.label}
+            </div>
+          </Tag>
+        </a>
+      ))}
+      {remainingTags.length > 0 && (
+        <>
+          <Tag
+            ref={moreTagsRef}
+            style={{
+              cursor: "pointer",
+            }}
+            design="Information"
+            hideStateIcon
+            id="moreTags"
+            onClick={() => {
+              setPopoverIsOpen(!popoverIsOpen);
+            }}
+            title="Show more tags"
+            onMouseEnter={(e) => {
+              e.currentTarget.style.textDecoration = "underline";
+            }}
+            onMouseLeave={(e) => {
+              e.currentTarget.style.textDecoration = "none";
+            }}
+            className={styles.tag}
+          >
+            <div style={{padding: "0px 2px 0px 2px"}}>
+            +{remainingTags.length}
+            </div>
+          </Tag>
+          <Popover
+            opener={moreTagsRef.current}
+            open={popoverIsOpen}
+            onClose={() => {
+              setPopoverIsOpen(false);
+            }}
+            style={{cursor: "initial"}}
+          >
+            <div style={{display: "flex", flexDirection: "Column", gap: "10px"}}>
+              {remainingTags.map((tag, index) => (
+                <a href={"/docs/tags/" + tag.tag} id="popover" key={index}>
+                  <Tag
+                    design="Information"
+                    hideStateIcon
+                    title={tag.description || tag.label}
+                    style={{borderRadius: "8px", cursor: "pointer"}}
+                    className={styles.tag}
+                  >
+                    {tag.label}
+                  </Tag>
+                </a>
+              ))}
+            </div>
+          </Popover>
+        </>
+      )}
     </FlexBox>
-    </FlexBox>
-      {/* Tags container */}
-      <FlexBox direction='Column' justifyContent='SpaceBetween' alignItems='Start' gap={10} style={{width: "100%", height: "65px"}}>
+    {/* Date and Link to open in a new window */}
+    <FlexBox direction="row" alignItems="End" justifyContent="SpaceBetween" style={{padding: "0px 16px 10px 16px", width: "100%"}}>
       <FlexBox
-        style={{
-          padding: '0 16px 0px 16px',
-          margin: 0,
-          bottom: 0,
-          width: "100%"
-        }}
-        direction='Row'
-        gap={3}
-        alignItems='End'
-        justifyContent='Start'
-      >
-        {compressedTags.map((tag, index) => (
-          <a href={"/docs/tags/" + tag.tag} key={index}>
-            <Tag
-              design="Information"
-              hideStateIcon
-              className={styles.tag}
-              title={tag.description || tag.label}
-              style={{borderRadius: "8px"}}
-            >
-              <div style={{textWrap: "nowrap"}}>
-                {tag.label}
-              </div>
-            </Tag>
-          </a>
-        ))}
-        {remainingTags.length > 0 && (
-          <>
-            <Tag
-              ref={moreTagsRef}
-              style={{
-                cursor: 'pointer',
-              }}
-              design='Information'
-              hideStateIcon
-              id="moreTags"
-              onClick={() => {
-                setPopoverIsOpen(!popoverIsOpen);
-              }}
-              title='Show more tags'
-              onMouseEnter={(e) => {
-                e.currentTarget.style.textDecoration = 'underline';
-              }}
-              onMouseLeave={(e) => {
-                e.currentTarget.style.textDecoration = 'none';
-              }}
-              className={styles.tag}
-            >
-              <div style={{padding: '0px 2px 0px 2px'}}>
-              +{remainingTags.length}
-              </div>
-            </Tag>
-            <Popover
-              opener={moreTagsRef.current}
-              open={popoverIsOpen}
-              onClose={() => {
-                setPopoverIsOpen(false);
-              }}
-              style={{cursor: "initial"}}
-            >
-              <div style={{display: "flex", flexDirection: "Column", gap: "10px"}}>
-                {remainingTags.map((tag, index) => (
-                  <a href={"/docs/tags/" + tag.tag} id="popover" key={index}>
-                    <Tag
-                      design="Information"
-                      hideStateIcon
-                      title={tag.description || tag.label}
-                      style={{borderRadius: "8px", cursor: "pointer"}}
-                      className={styles.tag}
-                    >
-                      {tag.label}
-                    </Tag>
-                  </a>
-                ))}
-              </div>
-            </Popover>
-          </>
-        )}
-      </FlexBox>
-      {/* Date */}
-      <FlexBox direction='row' alignItems='End' justifyContent='SpaceBetween' style={{padding: "0px 16px 10px 16px", margin: 0, width: "100%", bottom: 0}}>
-      <FlexBox
-      direction='Row'
-      alignItems='Center'
+      direction="Row"
+      alignItems="Center"
       style={{fontStyle: "italic"}}
       >
-        <Text style={{ cursor: "pointer", color: "gray", fontSize: 'var(--sapFontSmallSize)' }}>
-          {"Last Update: "}
+        <Text style={{ cursor: "pointer", color: "gray", fontSize: "var(--sapFontSmallSize)", paddingRight: 3 }}>
+          {"Last Update:"}
         </Text>
-        <Text style={{cursor: "pointer", paddingLeft: 3, color: "gray", fontSize: 'var(--sapFontSmallSize)'}}>
+        <Text style={{cursor: "pointer", paddingRight: 3, color: "gray", fontSize: "var(--sapFontSmallSize)"}}>
           {lastUpdate
-            ? new Date(lastUpdate).toLocaleDateString('en-US', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
+            ? new Date(lastUpdate).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
               })
-            : ''}
+            : ""}
         </Text>
       </FlexBox>
       <FlexBox
         style={{
-          cursor: 'pointer',
+          cursor: "pointer",
           paddingBottom: 2
         }}
         onClick={(e) => {
           e.stopPropagation();
-          window.open(href, '_blank');
+          window.open(href, "_blank");
         }}
       >
-        <Icon name="action" title="Open in a new window" style={{ color: '#0070F2', width: "15px" }} />
+        <Icon name="action" title="Open in a new window" style={{ color: "#0070F2", width: "15px" }} />
       </FlexBox>
-      </FlexBox>
-      </FlexBox>
-      </FlexBox>
+    </FlexBox>
+    </FlexBox>
+    </FlexBox>
     </Card>
   );
 }
@@ -362,13 +352,13 @@ function CardCategory({item}) {
 
 function CardLink({ item }) {
   const href = item.customProps?.href;
-  const description = item.description ?? item.customProps?.description ?? '';
+  const description = item.description ?? item.customProps?.description ?? "";
 
   return (
     <CardLayout
       href={item.href ?? href}
       title={item.customProps.title}
-      description={description.length > 300 ? description.substring(0, 300) + '...' : description}
+      description={description.length > 300 ? description.substring(0, 300) + "..." : description}
       tags={item.customProps.tags}
       lastUpdate={item.customProps.last_update}
       item={item}
@@ -379,9 +369,9 @@ function CardLink({ item }) {
 
 export default function DocCard({item}) {
   switch (item.type) {
-    case 'link':
+    case "link":
       return <CardLink item={item} />;
-    case 'category':
+    case "category":
       return <CardCategory item={item} />;
     default:
       throw new Error(`unknown item type ${JSON.stringify(item)}`);
