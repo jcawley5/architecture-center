0d115a3ab5e8ab37aa7d24292993c6951c15a058
1782GIT_USER <1782GIT_USER_EMAIL>
2025-05-06 12:02:55 +0000

    pr build ➝ gh pages

---
+++ @preview/navyakhurana-svg-optimization-fix/assets/js/acecf23e.a35f814e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/navyakhurana-svg-optimization-fix/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/navyakhurana-svg-optimization-fix/blog/authors"}')}}]);
