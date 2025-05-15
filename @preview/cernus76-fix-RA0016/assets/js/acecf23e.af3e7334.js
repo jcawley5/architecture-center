3087e461a2940d1a933ec58e38e60ae316cbe8f6
1973GIT_USER <1973GIT_USER_EMAIL>
2025-05-15 16:29:06 +0000

    pr build ➝ gh pages

---
+++ @preview/cernus76-fix-RA0016/assets/js/acecf23e.af3e7334.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/cernus76-fix-RA0016/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/cernus76-fix-RA0016/blog/authors"}')}}]);
