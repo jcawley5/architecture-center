358cf339f677a324c0fe254390ecf2888bc970c7
1832GIT_USER <1832GIT_USER_EMAIL>
2025-05-15 08:49:05 +0000

    pr build ➝ gh pages

---
+++ @preview/cernus76-update-anirbans-blog-post/assets/js/acecf23e.89561386.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/cernus76-update-anirbans-blog-post/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/cernus76-update-anirbans-blog-post/blog/authors"}')}}]);
