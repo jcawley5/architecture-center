4ab53a64c62ab2fc805c86418b3d19d2ff370df8
2151GIT_USER <2151GIT_USER_EMAIL>
2025-05-20 11:10:11 +0000

    pr build ➝ gh pages

---
+++ @preview/xammaxx-fix359/url-overload/assets/js/36994c47.7facbfdd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[89858],{45516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ @preview/xammaxx-fix359/url-overload/assets/js/acecf23e.e0f5878e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/xammaxx-fix359/url-overload/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/xammaxx-fix359/url-overload/blog/authors"}')}}]);
