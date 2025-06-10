f592f951edc7be34fadc19f0c4cf2e685b4fa5a2
Navya Khurana <64686397+navyakhurana@users.noreply.github.com>
2025-06-10 19:01:36 +0530

    Delete @preview/cernus76-main directory

--- @preview/cernus76-main/assets/js/acecf23e.164ed4a6.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/cernus76-main/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/cernus76-main/blog/authors"}')}}]);
