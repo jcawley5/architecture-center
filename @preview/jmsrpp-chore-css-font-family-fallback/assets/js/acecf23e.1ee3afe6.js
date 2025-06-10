8f43cb4c896dd8d5e8dc7a75c658e8f18322a56e
Navya Khurana <64686397+navyakhurana@users.noreply.github.com>
2025-06-10 19:02:24 +0530

    Delete @preview/jmsrpp-chore-css-font-family-fallback directory

--- @preview/jmsrpp-chore-css-font-family-fallback/assets/js/acecf23e.1ee3afe6.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/jmsrpp-chore-css-font-family-fallback/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/jmsrpp-chore-css-font-family-fallback/blog/authors"}')}}]);
