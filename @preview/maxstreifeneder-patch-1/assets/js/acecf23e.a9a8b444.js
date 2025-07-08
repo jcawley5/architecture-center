b417bc1119ee06a7f3a0d36a5dc9e7869d7a1584
2112GIT_USER <2112GIT_USER_EMAIL>
2025-07-08 09:05:25 +0000

    pr build ➝ gh pages

---
+++ @preview/maxstreifeneder-patch-1/assets/js/acecf23e.a9a8b444.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/maxstreifeneder-patch-1/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/maxstreifeneder-patch-1/blog/authors"}')}}]);
