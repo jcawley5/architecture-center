e2eb52e6527503bd6aac051f1a26230c36e2ae17
2089GIT_USER <2089GIT_USER_EMAIL>
2025-07-10 05:53:08 +0000

    pr build ➝ gh pages

---
+++ @preview/mahesh0431-dev/assets/js/acecf23e.45e420f8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksap_architecture_center=self.webpackChunksap_architecture_center||[]).push([[81903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/@preview/mahesh0431-dev/blog","blogTitle":"SAP Architecture Center - News","authorsListPath":"/@preview/mahesh0431-dev/blog/authors"}')}}]);
